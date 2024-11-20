7b56127f1fd85566049dda3af96aec662ab4a0b8
KarlaIsela <karlaiselasmile@gmail.com>
2024-11-20 13:18:57 -0600

    fix(logger_apps): fixed syntax errors in the logger file

--- api_apps/logger/logger_apps.py
+++ api_apps/logger/logger_apps.py
@@ -1,23 +1,22 @@
 import logging as log
 
 class Logger:
-    def _init_(self,log_file='api_apps.log', level=log.INFO):
-        log.basicConfing(
+    def __init__(self, log_file='api_apps.log', level=log.INFO): #
+        log.basicConfig( # 
             level=level, #Nivel de loggeo
-            format='%(asctime)s:%(levelname)s [%(filename)s:%(lineao)s] %(message)s', 
+            format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s', 
             datefmt='%I:%M:%S %p', 
             handlers=[ 
-                log.FileHandler(log_file)
+                log.FileHandler(log_file), # comma added
                 log.StreamHandler() 
             ]
         )
-        self.logger = log.getLogger()
-
+        self.logger = log
 
     def debug(self, message):
         self.logger.debug(message)
 
-    def into(self, message):
+    def info(self, message): # into -> info
         self.logger.info(message)
 
     def warning(self, message):
@@ -28,3 +27,11 @@ class Logger:
 
     def critical(self, message):
         self.logger.critical(message)
+
+if __name__=='__main__': 
+    logger = Logger()
+    logger.debug('Message level: DEBUG')
+    logger.info('Message level: INFO')
+    logger.warning('Message level: WARNING')
+    logger.error('Message level: ERROR')
+    logger.critical('Message level: CRITICAL')
